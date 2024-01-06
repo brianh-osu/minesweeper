82ae384f2ecb3e456e65d93413e94ea5e362bc11
Brian Hsiang <107973296+brianh-osu@users.noreply.github.com>
2024-01-05 16:29:43 -0800

    Add files via upload

---
+++ play-minesweeper.py
@@ -0,0 +1,40 @@
+import random 
+
+def play_Minesweeper():
+    def initialize_board():
+        board_len = int(input('How large would you like the board? Type an integer (i,e. 1 = 1x1 board, 2 = 2x2 board)'))
+        total_tiles  = board_len**2 
+        board = [['_' for x in range(board_len)] for x in range(board_len)] 
+        print('The board has been initialized as:')
+        return [board_len, total_tiles, board]
+    
+    def print_board():
+        for rows in board:
+            print(rows)
+
+    def place_mines():
+        num_mines = random.randint(1, 3) # number of mines 
+        print(f"There are a total of {total_tiles} tiles and {num_mines} mine(s).")
+        mine_positions = random.sample(range(1, total_tiles+1), num_mines) #array of mine positions
+        print(f"SECRET mine positions {mine_positions}")
+        for row in range(len(board)):
+            for col in range(len(board[row])): 
+                position_number = row * board_len + col + 1 
+                if position_number in mine_positions:
+                    board[row][col] = 'X'
+        print('Mines have been set.')
+        print_board()
+        
+    def uncover_mines():
+        choice = input("Which tile would you like to uncover?")
+        
+    # set global variables 
+    board = initialize_board()
+    board_len, total_tiles, board = board[0], board[1], board[2]
+    print_board()
+    place_mines()
+    uncover_mines()
+
+
+if __name__ == '__main__':
+    play_Minesweeper()
